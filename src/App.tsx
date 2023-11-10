532b0efe1f8fdede17697010cad66e33b7203f13
Sam Caldwell <samuelgrantcaldwell@gmail.com>
2023-11-10 01:17:20 -0800

    pushing build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import clapsound from './clap.wav'
 import kicksound from './kick.wav'
